4d3c56c192565ce4078bf2ea75ccc3cce6c5aeab
Shruthi <shruthi6040@gmail.com>
2024-07-26 16:52:57 +0530

    test ControlFlowTask

--- src/ControlFlow/Factorial.java
+++ src/ControlFlow/Factorial.java
@@ -1,13 +1,4 @@
 package ControlFlowTask;
 
  class Factorial {
-     public static void main(String args[])
-     {
-         int n=4;
-         int fact=1;
-         for(int i=0;i<n;i++){
-             fact=fact+fact*i;
-         }
-         System.out.println("Factorial of 4 is:"+fact);
-     }
-}
+ }
