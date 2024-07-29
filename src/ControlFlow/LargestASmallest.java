b281d83127794e33dca0508b32e4347c46164cb7
Shruthi <shruthi6040@gmail.com>
2024-07-29 10:32:38 +0530

    test ControlFlowTask

--- src/ControlFlow/LargestASmallest.java
+++ src/ControlFlow/LargestASmallest.java
@@ -1,20 +1,4 @@
 package ControlFlowTask;
 
  class LargestASmallest {
-     public static void main(String agrs[]){
-         int[] arr={42,445547,784857,145142,78448};
-         int n=arr.length;
-         for(int i=0;i<n;i++){
-             for(int j=i+1;j<n;j++){
-                 if(arr[i]<arr[j]){
-                     int t=arr[i];
-                     arr[i]=arr[j];
-                     arr[j]=t;
-                 }
-             }
-            // System.out.println(arr[i]);
-         }
-         System.out.println("Largest Number is:"+arr[0]);
-         System.out.println("Smallest Number is:"+arr[n-1]);
-     }
-}
+ }
