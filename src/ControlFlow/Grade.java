536e6a673d6c6d14ec636db1dfd235106456f48b
Shruthi <shruthi6040@gmail.com>
2024-07-29 10:49:55 +0530

    test ControlFlowTask

--- src/ControlFlow/Grade.java
+++ src/ControlFlow/Grade.java
@@ -1,4 +1,18 @@
 package ControlFlowTask;
 
  class Grade {
+  public static void main(String[] args) {
+   int Marks=489;
+   if(Marks>489){
+    System.out.println("Your grade is A");
+   } else if (Marks==489) {
+    System.out.println("Your grade is B");
+
+   }
+   else{
+    System.out.println("Your grade is C");
+   }
+  }
+
+
  }
