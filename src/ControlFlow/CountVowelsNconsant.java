00d50c1a20772407cfd2a3da98901019a25fff86
Shruthi <shruthi6040@gmail.com>
2024-08-01 16:47:11 +0530

    test ControlFlowTask

--- src/ControlFlow/CountVowelsNconsant.java
+++ src/ControlFlow/CountVowelsNconsant.java
@@ -1,22 +1,33 @@
 package ControlFlowTask;
 
  class CountVowelsNconsant {
-  public static void main(String args[]){
-   String fruits="Bannana";
-   int i;
-   int n=fruits.length();
-   int countVowels=0;
-   int countConsants=0;
-   for(i=0;i<n;i++){
-    if(fruits.charAt(i)=='a' || fruits.charAt(i)=='A'|| fruits.charAt(i)=='e' || fruits.charAt(i)=='E' || fruits.charAt(i)=='I' || fruits.charAt(i)=='i' || fruits.charAt(i)=='o' || fruits.charAt(i)=='O' || fruits.charAt(i)=='u' || fruits.charAt(i)=='U'){
-     countVowels=countVowels+1;
-    }
-    else{
-     countConsants=countConsants+1;
-    }
-   }
-   System.out.println("No Of Vowels:"+countVowels);
-   System.out.println("No of Consants:"+countConsants);
+     public static void main(String args[]) {
+         String[] array = {"Apple", "ball", "collection", "meeting"};
+         int i, j;
 
-  }
+
+         //int n=fruits.length();
+         for (i = 0; i < 4; i++) {
+             int countVowels=0;
+             int countConstants=0;
+
+
+             for (j = 0; j < array[i].length(); j++)
+                 if (array[i].charAt(j) == 'a' || array[i].charAt(j) == 'A' || array[i].charAt(j) == 'e' || array[i].charAt(j) == 'E' || array[i].charAt(j) == 'I' || array[i].charAt(j) == 'i' || array[i].charAt(j) == 'o' || array[i].charAt(j) == 'O' || array[i].charAt(j) == 'u' || array[i].charAt(j) == 'U') {
+                     countVowels++;
+
+
+                 }
+             else{
+                 countConstants++;
+                 }
+             System.out.println(array[i]+" has "+countVowels+" vowels"+" "+countConstants+" Constants");
+
+
+         }
+
+
+
+
+     }
  }
