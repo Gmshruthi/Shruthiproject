f8a36e9171f9c69c4767c6f3a56500b26e2fa720
Shruthi <shruthi6040@gmail.com>
2024-08-22 12:07:46 +0530

    test ControlFlowTask

--- src/practise/IncreasingOrDecreasingOrder.java
+++ src/practise/IncreasingOrDecreasingOrder.java
@@ -1,24 +1,25 @@
 package Git.src.practise;
 import java.util.Scanner;
 public class IncreasingOrDecreasingOrder {
-    public static void increasingOrDecreasingOrder(int number1,int number2,int number3){
+    public String increasingOrDecreasingOrder(int number1,int number2,int number3){
         if(number1<number2 && number2<number3){
-            System.out.println("In increasing order");
+            return "In increasing order";
         }
         else if(number1>number2 && number2>number3){
-            System.out.println("In decreasing order");
+            return "In decreasing order";
         }
         else{
-            System.out.println("Neither in increasing nor decreasing order");
+            return "Neither in increasing nor decreasing order";
         }
     }
         public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Enter the three numbers: ");
-            int number1=scanner.nextInt();
-            int number2=scanner.nextInt();
-            int number3=scanner.nextInt();
-            increasingOrDecreasingOrder(number1,number2,number3);
+        IncreasingOrDecreasingOrder increasingOrDecreasingOrder=new IncreasingOrDecreasingOrder();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the three numbers: ");
+        int number1=scanner.nextInt();
+        int number2=scanner.nextInt();
+        int number3=scanner.nextInt();
+        System.out.println(increasingOrDecreasingOrder.increasingOrDecreasingOrder(number1,number2,number3));
         }
     }
 
