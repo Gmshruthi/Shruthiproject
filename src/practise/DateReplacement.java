e1eadc389c50b9fb951576e80b87d6fddd43ac82
Shruthi <shruthi6040@gmail.com>
2024-09-10 14:05:41 +0530

    test ControlFlowTask

---
+++ src/practise/DateReplacement.java
@@ -0,0 +1,26 @@
+package Git.src.practise;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DateReplacement{
+    public String replaceDates(String text){
+        String dateRegex="(\\d{2})/(\\d{2})/(\\d{4})";
+        String replaceRegex = "$3-$2-$1";
+        Pattern pattern=Pattern.compile(dateRegex);
+        Matcher matcher=pattern.matcher(text);
+        return matcher.replaceAll(replaceRegex);
+    }
+    public static void main(String[] args) {
+        DateReplacement dateReplacement=new DateReplacement();
+        Scanner scanner=new Scanner(System.in);
+        String text= scanner.nextLine();
+        System.out.println(dateReplacement.replaceDates(text));
+    }
+}
+
+
+
+
+
