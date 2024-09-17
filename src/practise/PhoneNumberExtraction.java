b9262acdd56369602a032be98e68dd4bef6b2899
Shruthi <shruthi6040@gmail.com>
2024-09-17 17:42:26 +0530

    test ControlFlowTask

--- src/practise/PhoneNumberExtraction.java
+++ src/practise/PhoneNumberExtraction.java
@@ -6,16 +6,24 @@ import java.util.regex.Pattern;
 
 public class PhoneNumberExtraction {
     public  String extractPhoneNumbers(String Text){
-        Pattern pattern=Pattern.compile("\\(?\\+?\\d{2}\\)?\\s?\\d{5}-\\d{5}|\\d{3}-\\d{3}-\\d{4}");
-        Matcher matcher=pattern.matcher(Text);
-        if(matcher.find())
-            return matcher.group();
-        return null;
-    }
-    public static void main(String[] args) {
-        PhoneNumberExtraction phoneNumberExtraction=new PhoneNumberExtraction();
-        Scanner scanner = new Scanner(System.in);
-        String Text = scanner.nextLine();
-        System.out.println(phoneNumberExtraction.extractPhoneNumbers(Text));
+            String phoneNumberregex = "\\(\\+\\d{2}\\)\\s\\d{5}-\\d{5}|\\d{3}-\\d{3}-\\d{4}";
+            Pattern pattern = Pattern.compile(phoneNumberregex);
+            Matcher matcher = pattern.matcher(Text);
+            StringBuilder output=new StringBuilder();
+            while(matcher.find()){
+                if(!output.isEmpty()){
+                    output.append(" Or ");
+                }
+                output.append(matcher.group());
+            }
+            if(!output.isEmpty()){
+                return output.toString();
+            }
+            else
+                return "Null";
+        }
+        public static void main(String[] args) {
+            PhoneNumberExtraction phoneNumber=new PhoneNumberExtraction();
+            System.out.println(phoneNumber.extractPhoneNumbers("Call me at (+91) 23456-72890 or at 987-424-4124"));
     }
 }
