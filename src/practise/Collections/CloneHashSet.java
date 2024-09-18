fcebac3bcdf1ea45bdd97abf01e0cc496394576a
Shruthi <shruthi6040@gmail.com>
2024-09-18 17:24:14 +0530

    test ControlFlowTask

--- src/practise/Collections/CloneHashSet.java
+++ src/practise/Collections/CloneHashSet.java
@@ -1,16 +1,16 @@
 package Git.src.practise.Collections;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class CloneHashSet {
     public static void main(String[] args) {
-        HashMap<String,Integer> map=new HashMap<>();
-        map.put("a",1);
-        map.put("b",2);
-        map.put("c",3);
-        map.put("d",4);
-        HashMap copy = (HashMap)map.clone();
-        copy.remove("a");
+        HashSet<String> Set=new HashSet<>();
+        Set.add("a1");
+        Set.add("b");
+        Set.add("c");
+        Set.add("d");
+        HashSet copy = (HashSet)Set.clone();
         System.out.println(copy);
     }
 }
