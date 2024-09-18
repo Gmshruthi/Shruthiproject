b8245ef11becd029c723570ce7f0cb3fed8b3878
Shruthi <shruthi6040@gmail.com>
2024-09-18 14:25:48 +0530

    test ControlFlowTask

--- src/practise/Collections/CompareArrayLists.java
+++ src/practise/Collections/CompareArrayLists.java
@@ -10,14 +10,15 @@ public class CompareArrayLists {
         arrayList1.add(12);
         arrayList1.add(13);
         System.out.println("List1:"+arrayList1);
+        arrayList2.add(10);
         arrayList2.add(12);
-        arrayList2.add(11);
-        arrayList2.add(23);
+        arrayList2.add(0);
         System.out.println("List2:"+arrayList2);
-        arrayList1.retainAll(arrayList2);
-        System.out.println(arrayList1);
-
-
-
+        if(arrayList1.equals(arrayList2)){
+            System.out.println("Equal");
+        }
+        else{
+            System.out.println("Not Equal");
+        }
     }
 }
--- src/practise/Collections/InsertElement.java
+++ src/practise/Collections/InsertElement.java
@@ -5,7 +5,11 @@ import java.util.LinkedList;
 public class InsertElement {
     public static void main(String[] args) {
         LinkedList<Integer> linkedList=new LinkedList<>();
+        linkedList.add(12);
+        linkedList.add(13);
+        linkedList.add(5);
+        System.out.println("Original set:"+linkedList);
         linkedList.addFirst(1);
-        System.out.println(linkedList);
+        System.out.println("After Adding:"+linkedList);
     }
 }
