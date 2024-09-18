e2c36dcc9dfa0c8a961fcf5ce58c9f74f228be95
Shruthi <shruthi6040@gmail.com>
2024-09-18 11:35:07 +0530

    test ControlFlowTask

--- src/practise/Collections/AppendElementToHashSet.java
+++ src/practise/Collections/AppendElementToHashSet.java
@@ -5,17 +5,16 @@ import java.util.Scanner;
 
 public class AppendElementToHashSet {
     public static void main(String[] args) {
-        HashSet<Integer> set =new HashSet<>();
+        HashSet<Integer> set = new HashSet<>();
         set.add(1);
         set.add(2);
         set.add(4);
-        Scanner scanner=new Scanner(System.in);
-        int element= scanner.nextInt();
-        if(element>4){
+        Scanner scanner = new Scanner(System.in);
+        int element = scanner.nextInt();
+        if (element > 4) {
             set.add(element);
-            System.out.println("After append set:"+set);
-        }
-        else
+            System.out.println("After append set:" + set);
+        } else
             System.out.println("Enter greater than 4");
     }
 }
