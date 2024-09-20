37aaa6bc4289f10a47da0c10fdc6ce64ca31b2dd
Shruthi <shruthi6040@gmail.com>
2024-09-20 14:56:15 +0530

    test ControlFlowTask

--- src/practise/JDBC/SearchByTitle.java
+++ src/practise/JDBC/SearchByTitle.java
@@ -1,16 +1,21 @@
 package Git.src.practise.JDBC;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class SearchByTitle{
     public static void main(String[] args) {
         String url = "jdbc:sqlserver://192.168.0.49\\HEALNET;databaseName=Intern_ShruthiGM;encrypt=true;trustServerCertificate=true";
         String user = "shruthigm";
         String password = "Shruthi16";
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter title:");
+        String inputTitle=scanner.next();
         try {
             Connection connection = DriverManager.getConnection(url, user, password);
-            String sql="SELECT * FROM books WHERE title='marry gold'";
+            String sql="SELECT * FROM books WHERE title=?";
             PreparedStatement preparedStatement= connection.prepareStatement(sql);
+            preparedStatement.setString(1,inputTitle);
             ResultSet resultSet= preparedStatement.executeQuery();
             while(resultSet.next()) {
                 int id= resultSet.getInt("id");
