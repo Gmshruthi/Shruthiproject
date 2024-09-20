58b6add790e4b6a569e17e8eb1963c16bfc2034f
Shruthi <shruthi6040@gmail.com>
2024-09-20 14:34:36 +0530

    test ControlFlowTask

--- src/practise/JDBC/DeleteRecord.java
+++ src/practise/JDBC/DeleteRecord.java
@@ -1,16 +1,21 @@
 package Git.src.practise.JDBC;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class DeleteRecord{
     public static void main(String[] args) {
         String url = "jdbc:sqlserver://192.168.0.49\\HEALNET;databaseName=Intern_ShruthiGM;encrypt=true;trustServerCertificate=true";
         String user = "shruthigm";
         String password = "Shruthi16";
+        System.out.println("Enter  id:");
+        Scanner scanner=new Scanner(System.in);
+        int id= scanner.nextInt();
         try {
             Connection connection = DriverManager.getConnection(url, user, password);
-            String sql= "DELETE FROM books WHERE id=1";
+            String sql= "DELETE FROM books WHERE id=?";
             PreparedStatement preparedStatement= connection.prepareStatement(sql);
+            preparedStatement.setInt(1,id);
             preparedStatement.execute();
             System.out.println("record deleted");
             connection.close();
