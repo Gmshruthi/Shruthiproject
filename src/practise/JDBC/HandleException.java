51d82280b7fdacb42b383b21c442cb2349062efc
Shruthi <shruthi6040@gmail.com>
2024-09-23 11:21:40 +0530

    test ControlFlowTask

---
+++ src/practise/JDBC/HandleException.java
@@ -0,0 +1,29 @@
+package Git.src.practise.JDBC;
+
+import java.sql.*;
+
+public class HandleException{
+        public static void main(String[] args) {
+            String url = "jdbc:sqlserver://192.168.0.49\\HEALNET;databaseName=Intern_ShruthiGM;encrypt=true;trustServerCertificate=true";
+            String user = "shruthigm";
+            String password = "Shruthi16";
+            try {
+                Connection connection = DriverManager.getConnection(url, user, password);
+                String sql="SELECT * FROM manual";
+                PreparedStatement preparedStatement= connection.prepareStatement(sql);
+                ResultSet resultSet= preparedStatement.executeQuery();
+                System.out.println("id\t\ttitle\t\tauthor");
+                while (resultSet.next()){
+                    int id= resultSet.getInt("id");
+                    String title= resultSet.getString("title");
+                    String author= resultSet.getString("author");
+                    System.out.println(id+ "\t\t" + title+ "\t\t" +author);
+                }
+            } catch (SQLException e) {
+                handleSQLException(e);
+            }
+        }
+        static void handleSQLException(SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
